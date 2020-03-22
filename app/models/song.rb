29e779a2b572f61af6888e1258d85b499653c646
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-22 23:47:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,9 @@
 class Song < ActiveRecord::Base
   # add associations here
-  accepts_nested_attributes_for :notes
   belongs_to :artist
   belongs_to :genre
   has_many :notes
+  accepts_nested_attributes_for :notes
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
