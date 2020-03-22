6791fd58b21e07a6148f18fd77287bd1c5e7cd90
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-22 23:44:28 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,40 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  # def note_contents=(contents)
+  #   contents.each do |content|
+  #     if !content.empty?
+  #         note = Note.create(content: content, song_id: self.id)
+  #         self.notes << note
+  #     end
+  #   end
+  # end
+  def note_contents=(contents)
+   contents.delete_if(&:blank?).each { |content|
+     if !Note.find_by(content: content, song_id: self.id)
+     self.notes.build(content: content)
+   end
+   }
+  end
+
+  def note_contents
+    self.notes.map { |note| note.content }
+  end
+
+  # def note_contents=(contents)
+  #   contents.each do |content|
+  #     self.notes.build(content: content).save if !!content
+  #   end
+  # end
 end
